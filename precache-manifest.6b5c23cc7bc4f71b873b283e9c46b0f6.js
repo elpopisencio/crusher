85a4493467e653f0785cb6ea60c7c4e1c7b90da3
Codea IT <codeait@MacBookPros-MacBook-Pro.local>
2020-02-29 21:29:12 -0300

    Updates

---
+++ precache-manifest.6b5c23cc7bc4f71b873b283e9c46b0f6.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d9d3ab1a089982c290a3557315db42d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b649ad7498a3c8da0b4f",
+    "url": "/static/css/main.7d7a7dae.chunk.css"
+  },
+  {
+    "revision": "2c51102d8737e9cea3d8",
+    "url": "/static/js/2.a7bb9eb1.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.a7bb9eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b649ad7498a3c8da0b4f",
+    "url": "/static/js/main.86c349f8.chunk.js"
+  },
+  {
+    "revision": "04fa740b09382ba9eecf",
+    "url": "/static/js/runtime-main.06203194.js"
+  },
+  {
+    "revision": "43b7ec96bb3b511a59d9467d4d92bda1",
+    "url": "/static/media/011-core.43b7ec96.svg"
+  },
+  {
+    "revision": "8cc28b50acf9ba62bac27b8be44b8b7a",
+    "url": "/static/media/014-bottle.8cc28b50.svg"
+  },
+  {
+    "revision": "6969b894b0d6bca1b7b2c0111b0188da",
+    "url": "/static/media/015-can.6969b894.svg"
+  },
+  {
+    "revision": "cfa17003f716fadee324c02a23e19dd5",
+    "url": "/static/media/016-banana.cfa17003.svg"
+  },
+  {
+    "revision": "2c149c9b531631179a5a9a451267f5f4",
+    "url": "/static/media/017-bones.2c149c9b.svg"
+  },
+  {
+    "revision": "40e5737da84587a59f7ea6c5512f85b6",
+    "url": "/static/media/047-bulb.40e5737d.svg"
+  },
+  {
+    "revision": "9531f21c32eefdee8cb1390d22ecca1a",
+    "url": "/static/media/049-jumper.9531f21c.svg"
+  },
+  {
+    "revision": "203d74bfc73fbcf1de9f0ed4cff3df64",
+    "url": "/static/media/050-milk carton.203d74bf.svg"
+  },
+  {
+    "revision": "c391276b14a21f66df1d78687b77917b",
+    "url": "/static/media/052-teddy.c391276b.svg"
+  },
+  {
+    "revision": "57ec0c81687fa435784f919b4750d560",
+    "url": "/static/media/061-rubbish.57ec0c81.svg"
+  },
+  {
+    "revision": "85d479aebb1770789e487eaf3f2928c3",
+    "url": "/static/media/black.85d479ae.svg"
+  },
+  {
+    "revision": "2272066cdec99add0e1aab86d3b8275c",
+    "url": "/static/media/blue.2272066c.svg"
+  },
+  {
+    "revision": "fa74ac1e0d7e31b0eb47d0a0c9091b6b",
+    "url": "/static/media/brown.fa74ac1e.svg"
+  },
+  {
+    "revision": "06a56446396b4e09f11bcbddfd21994b",
+    "url": "/static/media/green.06a56446.svg"
+  },
+  {
+    "revision": "ad7ab918db7fbe44c675e6e7da04ddaf",
+    "url": "/static/media/orange.ad7ab918.svg"
+  },
+  {
+    "revision": "6d7dd2fac3ac3e3109b3c3e0e0dbb2e4",
+    "url": "/static/media/red.6d7dd2fa.svg"
+  },
+  {
+    "revision": "6131dcd585d8821f25e4068078623a5d",
+    "url": "/static/media/yellow.6131dcd5.svg"
+  }
+]);
